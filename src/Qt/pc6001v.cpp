0e989db3a7cc8eaa39e08f985f6874c61f3bc9a6
Tadahito Yao <eighttails@nifty.com>
2013-04-19 22:21:34 +0900

    UTF8toLocalで落ちるのを修正

--- src/Qt/pc6001v.cpp
+++ src/Qt/pc6001v.cpp
@@ -5,13 +5,6 @@
 #include <QTimer>
 #include <QDir>
 
-#ifdef WIN32
-//Windowsではプラグインを静的リンクする。
-#include <QtPlugin>
-Q_IMPORT_PLUGIN(qjpcodecs)
-Q_IMPORT_PLUGIN(qico)
-#endif
-
 #include "qtp6vxapplication.h"
 
 ///////////////////////////////////////////////////////////
@@ -19,16 +12,14 @@ Q_IMPORT_PLUGIN(qico)
 ///////////////////////////////////////////////////////////
 int main( int argc, char *argv[] )
 {
-    QCoreApplication::setAttribute(Qt::AA_X11InitThreads);
+    //X11の場合
+    if (QGuiApplication::platformName() == QLatin1String("xcb")){
+        QCoreApplication::setAttribute(Qt::AA_X11InitThreads);
+    }
 
     QtP6VXApplication app(argc, argv);
 
     setlocale(LC_ALL,"Japanese");
-
-#ifdef WIN32
-    // Windowsではアプリに同梱のqjpcodecs4.dllプラグインを読み込むため、そのパスを指定
-    QApplication::addLibraryPath(qApp->applicationDirPath() + QDir::separator() + "plugins");
-#endif
 
     QTimer::singleShot(100, &app, SLOT(startup()));
     app.exec();

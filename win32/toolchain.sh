ab573ee2fe8748c3b2f83a27b6b7efdc4475e890
Tadahito Yao <eighttails@nifty.com>
2014-04-10 23:27:14 +0900

    リリース用スクリプトを追加

--- win32/toolchain.sh
+++ win32/toolchain.sh
@@ -1,4 +1,4 @@
 #ƒc[ƒ‹—Ş
-pacman -Su base base-devel mingw-w64-i686-toolchain VCS unzip wget tar perl python ruby mingw-w64-i686-icu  mingw-w64-i686-SDL2
+pacman -Su base base-devel mingw-w64-i686-toolchain VCS unzip wget tar zip perl python ruby mingw-w64-i686-icu  mingw-w64-i686-SDL2
 
 

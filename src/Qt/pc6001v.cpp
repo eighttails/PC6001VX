9933e557fa08c13de62b350623e076a26c5afb87
Tadahito Yao <eighttails@nifty.com>
2014-06-03 20:38:55 +0900

    アプリケーションフラグを修正

--- src/Qt/pc6001v.cpp
+++ src/Qt/pc6001v.cpp
@@ -4,24 +4,17 @@
 
 #include "qtp6vxapplication.h"
 
-#ifndef NOJOYSTICK
-//リンクエラー解消のために入れておく必要がある
-#include <SDL2/SDL.h>
-#undef main
-#endif
-
 ///////////////////////////////////////////////////////////
 // メイン
 ///////////////////////////////////////////////////////////
 int main( int argc, char *argv[] )
 {
-    //X11の場合
-    if (QGuiApplication::platformName() == QLatin1String("xcb")){
-        QCoreApplication::setAttribute(Qt::AA_X11InitThreads);
-    }
-    QtP6VXApplication app(argc, argv);
+	//X11の場合用
+	QCoreApplication::setAttribute(Qt::AA_X11InitThreads);
 
-    QLocale locale;
+	QtP6VXApplication app(argc, argv);
+
+	QLocale locale;
     QString lang = locale.uiLanguages()[0];
     QTranslator myappTranslator;
 

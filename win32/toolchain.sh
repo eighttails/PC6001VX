535545c37caf80973df434f001ff30d10a07d3ee
Tadahito Yao <eighttails@nifty.com>
2016-11-28 05:58:53 +0900

    mingw32フォルダがなかった場合に作成するように変更

--- win32/toolchain.sh
+++ win32/toolchain.sh
@@ -1,2 +1,6 @@
+#ディレクトリが存在しない場合があるので作っておく
+mkdir /mingw32 > /dev/null
+mkdir /mingw64 > /dev/null
+
 #ツール類
-pacman -Suu --needed --noconfirm base base-devel mingw-w64-i686-toolchain VCS unzip wget tar zip perl python ruby mingw-w64-i686-icu  mingw-w64-i686-SDL2 mingw-w64-i686-libvorbis mingw-w64-i686-libvpx mingw-w64-i686-yasm
+pacman -S --needed --noconfirm base base-devel mingw-w64-i686-toolchain VCS unzip wget tar zip perl python ruby mingw-w64-i686-icu  mingw-w64-i686-SDL2 mingw-w64-i686-libvorbis mingw-w64-i686-libvpx mingw-w64-i686-yasm

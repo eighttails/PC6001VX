ac62b9609c833664b5a0b7f09747aee9eadcff6e
Tadahito Yao <eighttails@nifty.com>
2017-03-12 22:33:21 +0900

    Submoduleを削除

--- win32/buildenv.sh
+++ win32/buildenv.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#基本設定
-source MSYS2Private/common/common.sh
-commonSetup
 
 #このスクリプトの置き場所
 SCRIPT_DIR=$(dirname $(readlink -f ${BASH_SOURCE:-$0}))
+
+#基本設定
+source $SCRIPT_DIR/MSYS2Private/common/common.sh
+commonSetup
 
 #Qtをビルド
 #OpenGLにAngleを使用、バイナリサイズ抑制最適化
@@ -13,3 +14,5 @@ bash $SCRIPT_DIR/MSYS2Private/qt/qt.sh
 #FFmpegをビルド
 #P6VX用に必要最低限のCODECのみ有効化しサイズを削減したビルド
 bash $SCRIPT_DIR/MSYS2Private/ffmpeg/ffmpeg.sh
+
+

51372347b3a72b6fa71042c1a21dcd20a7c658de
Tadahito Yao <eighttails@nifty.com>
2018-05-04 20:49:14 +0900

    SVGファイルをPNGに差し替え

---
+++ src/Qt/virtualkeyitem.cpp
@@ -0,0 +1,6 @@
+#include "virtualkeyitem.h"
+
+VirtualKeyItem::VirtualKeyItem()
+{
+
+}
---
+++ src/Qt/virtualkeyitem.h
@@ -0,0 +1,11 @@
+#ifndef VIRTUALKEYITEM_H
+#define VIRTUALKEYITEM_H
+
+
+class VirtualKeyItem : public QGraphicsPixmapItem
+{
+public:
+	VirtualKeyItem();
+};
+
+#endif // VIRTUALKEYITEM_H
